6c7a28febcd1371ad91a9d5e1999de51675d6130
BXM22 <brennen@csuf.fullerton.edu>
2024-10-29 18:19:19 -0700

    added_args

---
+++ stream.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <inp> <cmd> <out>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    char *inp = argv[1];
+    char *cmd = argv[2];
+    char *out = argv[3];
+
+    
+
+    return 0;
+}
